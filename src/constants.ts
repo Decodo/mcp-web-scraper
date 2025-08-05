2e0703a12f7779d35983a97601cc7ac9022dae45
domantas-jurkus-dcd <domantas.jurkus@decodo.com>
2025-08-05 11:55:12 +0300

    feat: add reddit post tool (#14)

--- src/constants.ts
+++ src/constants.ts
@@ -1,5 +1,7 @@
 export enum SCRAPER_API_TARGETS {
   GOOGLE_SEARCH = 'google_search',
   AMAZON_SEARCH = 'amazon_search',
+
+  REDDIT_POST = 'reddit_post',
   REDDIT_SUBREDDIT = 'reddit_subreddit',
 }
---
+++ src/tools/index.ts
@@ -0,0 +1,5 @@
+export * from './amazon-search-parsed-tool';
+export * from './google-search-parsed-tool';
+export * from './reddit-subreddit-tool';
+export * from './reddit-post-tool';
+export * from './scrape-as-markdown-tool';
